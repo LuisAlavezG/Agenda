45c74d4c3bd170624885bad75732f30af9d92579
LuisAlavezG <luisarturogarciaalavez@gmail.com>
2024-06-06 15:52:58 -0600

    Se añade fullCalendar

--- index.php
+++ index.php
@@ -2,20 +2,40 @@
 <html lang='en'>
   <head>
     <meta charset='utf-8' />
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js'></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src= 'calendar/index.global.min.js' ></script>  
     <script>
 
       document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
+          initialView: 'dayGridMonth',
+          locale:"es",
+          headerToolbar:{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }
         });
         calendar.render();
       });
 
+      
     </script>
   </head>
   <body>
-    <div id='calendar'></div>
+
+
+      <div class="container">
+        <div class="col">
+            <div class="col-7">
+                <div id='calendar'></div>
+            </div>
+        </div>
+      </div>
+
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
   </body>
 </html>
